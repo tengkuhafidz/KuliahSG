973e58f9ff22120ce8ba1da852cdcb2b04c34daa
Tengku Hafidz <muhammad_hafidzuddin@tech.gov.sg>
2021-04-10 23:58:26 +0800

    enhance hero and navbar section

--- components/Layout/hero.tsx
+++ components/Layout/hero.tsx
@@ -1,14 +1,42 @@
 import {useContext} from 'react'
 import PageDataContext from '../../utils/PageDataContext'
+import Link from 'next/link'
 
 export default function Hero() {
-	const {heroTitle, heroDescription} = useContext(PageDataContext)
+	const {
+		heroTitle,
+		heroDescription,
+		primaryButtonText,
+		primaryButtonUrl,
+		secondaryButtonText,
+		secondaryButtonUrl,
+	} = useContext(PageDataContext)
+	console.log('primaryButtonUrl', primaryButtonUrl)
+	let firstButtonStyle =
+		'py-2 px-8 bg-green-500 text-white rounded-lg border border-green-500 hover:bg-green-700 hover:border-green-700 mr-4'
+	let secondButtonStyle =
+		'py-2 px-8 text-green-500 rounded-lg border border-green-500 hover:bg-green-700 hover:border-green-700 mr-4'
+	if (primaryButtonUrl === '/') {
+		firstButtonStyle =
+			'py-2 px-8 text-green-500 rounded-lg border border-green-500 hover:bg-green-700 hover:border-green-700 mr-4'
+		secondButtonStyle =
+			'py-2 px-8 bg-green-500 text-white rounded-lg border border-green-500 hover:bg-green-700 hover:border-green-700 mr-4'
+	}
+
 	return (
 		<div className="py-12 mx-auto max-w-2xl text-center">
 			<h1 className="text-3xl md:text-4xl font-bold text-white">{heroTitle}</h1>
 			<p className="py-6 font-light text-md text-gray-300 dark:text-gray-400">
 				{heroDescription}
 			</p>
+			<div className="mt-4">
+				<Link href={primaryButtonUrl}>
+					<button className={firstButtonStyle}>{primaryButtonText}</button>
+				</Link>
+				<Link href={secondaryButtonUrl}>
+					<button className={secondButtonStyle}>{secondaryButtonText}</button>
+				</Link>
+			</div>
 		</div>
 	)
 }
--- components/Layout/navbar.tsx
+++ components/Layout/navbar.tsx
@@ -2,11 +2,7 @@ import {useContext} from 'react'
 import PageDataContext from '../../utils/PageDataContext'
 
 export default function Navbar() {
-	const {
-		navButtonText,
-		navButtonUrl,
-		logoUrl,
-	} = useContext(PageDataContext)
+	const {navButtonText, navButtonUrl, logoUrl} = useContext(PageDataContext)
 
 	const renderNavButton = () => {
 		if (!navButtonText || !navButtonUrl) {
@@ -15,7 +11,7 @@ export default function Navbar() {
 
 		return (
 			<button
-				className={`ml-4 md:ml-8 py-1 px-3 md:py-2 md:px-6 rounded-md text-white bg-green-600  hover:bg-green-700`}
+				className={`ml-4 md:ml-8 py-1 px-3 md:py-2 md:px-6 rounded-md text-white border border-white  hover:bg-green-600 hover:text-white hover:border-green-600`}
 			>
 				<a href={navButtonUrl} target="_blank">
 					<span>{navButtonText}</span>
@@ -28,18 +24,10 @@ export default function Navbar() {
 		<div className="flex mt-2">
 			<div className="flex-grow">
 				<div>
-					<img
-						src={logoUrl}
-						alt="logo"
-						width={40}
-						height={40}
-						className="inline-block"
-					/>
+					<img src={logoUrl} alt="logo" height={40} className="inline-block" />
 				</div>
 			</div>
-			<div>
-				{renderNavButton()}
-			</div>
+			<div>{renderNavButton()}</div>
 		</div>
 	)
 }
